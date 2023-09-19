31cc00032919123e045c1631febeea58c6d4327c
Charlotte Fritz <133656144+charlottepfritz@users.noreply.github.com>
2023-09-18 23:45:27 -0400

    Update and rename Notes.js.rtf to Notes.js

---
+++ Notes.js
@@ -0,0 +1,37 @@
+class Notes {
+
+  constructor() {
+  }
+
+  assignNote(n, r, g, b, type, shift) {
+    this.n = n; //assign positions
+    this.type = type; //type, either player or opponent
+    this.r = r; //colour
+    this.g = g;
+    this.b = b;
+    this.s = shift; //shift for drawing
+  }
+
+  drawNote() {
+    for (var i = 0; i < this.n.length; i++) {
+      fill(this.r, this.g, this.b);
+      var p = { x: (int(this.n[i].x)), y: this.n[i].y }
+      ellipse(p.x * 55 + this.s, this.n[i].y, 50, 50);
+      this.n[i].y++; //y posiiton increases, moving notes down
+    }
+  }
+
+  checkDist() {
+    for (let i = 0; i < this.n.length; i++) {
+      for (let k = 0; k < 4; k++) {
+        if (this.n[i].y > 500 && this.n[i].y < 540) {
+          this.n[i].z = true; //if the note is in range, then true
+        }
+        else {
+          this.n[i].z = false; //if the note is not in range, false
+        }
+      }
+    }
+  }
+
+}
